401d585b1683694e1c9bad0be6767524afa58f4f
Benjamin Rowe <browe495@gmail.com>
2020-05-05 03:46:41 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
-# Write your methods here
+def loop_message_five_times(string)
+  for i in (1..5) do
+    puts string 
+  end 
+end 
+
+def loop_message_n_times(string, n)
+  i = 0 
+  while i < n do 
+    puts string 
+    i += 1 
+  end
+end 
+
+def output_array(array)
+  array.each { |x| puts x }
+end 
+
+def return_string_array(array)
+  array.reduce([]) { |memo, x| memo << x.to_s }
+end 
