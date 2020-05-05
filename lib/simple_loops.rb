fff30b183d382dc0fb55093a3517e8b0a06e0ed3
Benjamin Rowe <browe495@gmail.com>
2020-05-05 03:42:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
-# Write your methods here
+def loop_message_five_times(string)
+  for i in (1..5) do
+    puts string 
+  end 
+end 
+
+def loop_message_n_times(string, n)
+  i = 0 
+  while i < n do 
+    puts string 
+    i += 1 
+  end
+end 
+
+def output_array(array)
+  
+end 
